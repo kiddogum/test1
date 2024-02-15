3359c55685a70ceb6a3b537fc4629f79d319611b
kiddogum <123975377+kiddogum@users.noreply.github.com>
2024-02-16 00:45:01 +0700

    game 1.2

---
+++ config.js
@@ -0,0 +1,42 @@
+let xp = 0
+let health = 100
+let gold = 50
+let currentWeapon = 0
+const weapons = [
+    {
+        name : "stick",
+        power : 10
+    },
+    {
+        name : "dagger",
+        power : 50
+    },
+    {
+        name : "sword",
+        power : 100
+    },
+]
+
+const monsters = [
+    {
+        name : "slime",
+        power : 20,
+        health : 50
+    },
+    {
+        name : "wolf",
+        power : 50,
+        health : 100
+    },
+    {
+        name : "dragon",
+        power : 100,
+        health : 300
+    }
+]
+
+
+
+function goStore(){
+
+}
